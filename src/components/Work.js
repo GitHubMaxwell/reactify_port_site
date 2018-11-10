3b1301a8cb2bbd72b39b155f8658c6bcab61c8ee
Maxwell Rediker <maxwell.rediker@gmail.com>
2018-11-09 16:25:57 -0800

    troubleshooting webpack and method of image export so that images not broken in deployment

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,9 +1,11 @@
 import React from "react";
-import project1 from "../assets/projects/project1.jpg";
-import project2 from "../assets/projects/project2.jpg";
-import project3 from "../assets/projects/project3.jpg";
-import project4 from "../assets/projects/project4.jpg";
-import project5 from "../assets/projects/project5.jpg";
+import {
+  project1,
+  project2,
+  project3,
+  project4,
+  project5
+} from "../assets/assets_export.js";
 
 export default function Work() {
   return (
